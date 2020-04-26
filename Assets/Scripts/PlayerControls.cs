690eec6033d5f3d14e78c56dea27a7647688f0bf
DavisBoz <dboswor2@uoregon.edu>
2020-04-25 17:45:02 -0700

    small changes, started working on camera

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public float speed;
+    private int Jumped;
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (rb.transform.position.y == 0.5f)
+            Jumped = 0;
+        if (Input.GetKeyDown("space") && Jumped < 2)
+        {
+            Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
+            rb.AddForce(jump);
+            Jumped += 1;
+        }
+    }
+
+
+}
