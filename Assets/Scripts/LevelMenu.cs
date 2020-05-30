90437a1b1ed41ca6ec284fd618cb9896257a4387
DavisBoz <dboswor2@uoregon.edu>
2020-05-30 14:40:44 -0700

    Added achievements menu and level select menu to the main menu.

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void PlayLvl1()
+    {
+        SceneManager.LoadScene("Lvl 1");
+    }
+
+    public void PlayLvl2()
+    {
+        SceneManager.LoadScene("Lvl 2");
+    }
+
+    public void PlayLvl3()
+    {
+        SceneManager.LoadScene("Lvl 3");
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,20 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayLvl1()
-    {
-        SceneManager.LoadScene("Lvl 1");
-    }
-
-    public void PlayLvl2()
-    {
-        SceneManager.LoadScene("Lvl 2");
-    }
-
-    public void PlayLvl3()
-    {
-        SceneManager.LoadScene("Lvl 3");
-    }
 
     public void PlayTutorial()
     {
