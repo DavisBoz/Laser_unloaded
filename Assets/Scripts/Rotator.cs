31e477a72c27ba1bb781e73eb27e6c88a743c400
DavisBoz <dboswor2@uoregon.edu>
2020-05-31 01:24:37 -0700

    Rotator Added, Leve3 Created

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,9 +2,11 @@
 
 public class Rotator : MonoBehaviour
 {
+    public float mult;
+
 
     void Update()
     {
-        transform.Rotate(new Vector3(0, 180, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 180, 0) * Time.deltaTime  * mult);
     }
 }
