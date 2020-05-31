88b6539f15b1074727a824956d2deae4ead1d6db
DavisBoz <dboswor2@uoregon.edu>
2020-05-31 15:37:38 -0700

    Final fixes

--- Assets/Scripts/AchievementControls.cs
+++ Assets/Scripts/AchievementControls.cs
@@ -23,9 +23,9 @@ public class AchievementControls : MonoBehaviour
     void Update()
     {
         bool are_all_enemies_dead = EnemyHealth.count == 0;
-        bool ace = ScoreManager.score == 100;
-        bool ace_2 = ScoreManager.score == 200;
-        bool ace_3 = ScoreManager.score == 300;
+        bool ace = ScoreManager.score >= 100;
+        bool ace_2 = ScoreManager.score >= 200;
+        bool ace_3 = ScoreManager.score >= 300;
         bool all_pow = PlayerControls.count == pickup_ct;
 
         if (are_all_enemies_dead)
@@ -37,21 +37,21 @@ public class AchievementControls : MonoBehaviour
 
         if (ace)
         {
-            text.text = "You got to 100 points!";
+            text.text = "Ace ~ You got to 100 points!";
             ach_box.SetActive(true);
             StartCoroutine(Disappear(ach_box, 5.0f)); // 5 seconds
         }
 
         if (ace_2)
         {
-            text.text = "You got to 200 points!";
+            text.text = "Mad Man ~  You got to 200 points!";
             ach_box.SetActive(true);
             StartCoroutine(Disappear(ach_box, 5.0f)); // 5 seconds
         }
 
         if (ace_3)
         {
-            text.text = "You got to 300 points!";
+            text.text = "Mastery ~ You got to 300 points!";
             ach_box.SetActive(true);
             StartCoroutine(Disappear(ach_box, 5.0f)); // 5 seconds
         }
